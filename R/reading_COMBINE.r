ee5e828af9a03c8e063f8f5297786fe2b0947cb2
Alban Sagouis <alban.sagouis@idiv.de>
2022-06-02 15:05:56 +0200

    fixing + splitting reading_COMBINE into 2 functions

--- R/reading_COMBINE.r
+++ R/reading_COMBINE.r
@@ -1,4 +1,4 @@
-#' Reading COMBINE data
+#' Reading COMBINE raw data
 #'
 #' @description Reading, after downloading if needed, the trait data from:
 #' Soria, C.D., Pacifici, M., Di Marco, M., Stephen, S.M. and Rondinini, C.
@@ -10,22 +10,38 @@
 #'
 
 
-reading_COMBINE <- function() {
+reading_COMBINE_raw <- function() {
    create_folders()
-   # downloading from figshare, unzipping, deleting the archive.
-   if (!file.exists('./data/downloaded_data/traits/COMBINE_10.1002_ecy.3344_1/MetadataS1.pdf')) {
-      suppdata::suppdata(x = '10.1002/ecy.3344', si = 1, from = 'wiley',
-                         dir = './data/cache/traits', save.name = 'COMBINE_10.1002_ecy.3344_1.zip')
-      unzip(zipfile = './data/cache/traits/COMBINE_10.1002_ecy.3344_1.zip',
-            exdir = './data/downloaded_data/traits/COMBINE_10.1002_ecy.3344_1')
-
-      unzip(zipfile = './data/downloaded_data/traits/COMBINE_10.1002_ecy.3344_1/ecy3344-sup-0001-datas1.zip',
-            exdir = './data/downloaded_data/traits/COMBINE_10.1002_ecy.3344_1')
-      file.remove('./data/downloaded_data/traits/COMBINE_10.1002_ecy.3344_1/ecy3344-sup-0001-datas1.zip')
-   }
-
-   # reading the csv
    return(
-      data.table::fread('./data/downloaded_data/traits/COMBINE_10.1002_ecy.3344_1/COMBINE_archives/trait_data_imputed.csv', sep = ',', dec = '.', header = TRUE)
+      data.table::fread(
+         file = suppdata::suppdata(
+            x = '10.6084/m9.figshare.13028255.v4', si = 18, from = 'figshare',
+            dir = './data/downloaded_data/traits', save.name = 'COMBINE_trait_data_reported.csv'),
+         sep = ',', dec = '.', header = TRUE
+         )
+   )
+}
+
+#' Reading COMBINE imputed data
+#'
+#' @description Reading, after downloading if needed, the trait data from:
+#' Soria, C.D., Pacifici, M., Di Marco, M., Stephen, S.M. and Rondinini, C.
+#' (2021), COMBINE: a coalesced mammal database of intrinsic and extrinsic
+#' traits. Ecology. Accepted Author Manuscript e03344.
+#' https://doi.org/10.1002/ecy.3344
+#' @return A \code{data.table}, for mammals
+#' @export
+#'
+
+
+reading_COMBINE_imputed <- function() {
+   create_folders()
+   return(
+      data.table::fread(
+         file = suppdata::suppdata(
+            x = '10.6084/m9.figshare.13028255.v4', si = 19, from = 'figshare',
+            dir = './data/downloaded_data/traits', save.name = 'COMBINE_trait_data_imputed.csv'),
+         sep = ',', dec = '.', header = TRUE
+      )
    )
 }
